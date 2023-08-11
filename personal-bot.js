5d21a9528b120a340db4ad6aa371b4b7650965b4
unknown <mohammadhg98@gmail.com>
2023-08-11 03:18:19 -0700

    conversation options added

--- personal-bot.js
+++ personal-bot.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.personalChatChain = void 0;
 const openai_1 = require("langchain/chat_models/openai");
@@ -20,81 +22,87 @@ let vectorStore;
 const apiKey = config_1.OPENAI_API_KEY;
 const filePath = path_1.default.join(__dirname, "public", "biography.txt");
 
-
-
-
 // Create docs with a loader
 const loader = new text_1.TextLoader(filePath);
 (async () => {
-    
-    const docs = await loader.load();
-    vectorStore = await hnswlib_1.HNSWLib.fromDocuments(docs, new openai_2.OpenAIEmbeddings({ openAIApiKey: apiKey }));
+  const docs = await loader.load();
+  vectorStore = await hnswlib_1.HNSWLib.fromDocuments(
+    docs,
+    new openai_2.OpenAIEmbeddings({ openAIApiKey: apiKey })
+  );
 })();
 const initializeChain = async (initialPrompt) => {
-    const model = new openai_1.ChatOpenAI({
-        temperature: 0.3,
-        modelName: "gpt-3.5-turbo",
-        openAIApiKey: apiKey,
-    });
+  const model = new openai_1.ChatOpenAI({
+    temperature: 0.9,
+    modelName: "gpt-3.5-turbo",
+    openAIApiKey: apiKey,
+  });
 
-    //HNSWLib
-    // Load the docs into the vector store
-    //    const fileContent= readFileContent(filePath);
-    //    const splitter = new CharacterTextSplitter({
-    //     separator: " ",
-    //     chunkSize: 15,
-    //     chunkOverlap: 3,
-    //   });
-    //   const output = await splitter.createDocuments([fileContent]);
-    // const directory = path.join(__dirname + "/public");
-    // await vectorStore.save(directory);
-    // const loadedVectorStore=await HNSWLib.load(directory,new OpenAIEmbeddings({openAIApiKey:apiKey}));
-    chain = chains_1.ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
-        verbose: false
-    });
-    const response = await chain.call({
-        question: initialPrompt,
-        chat_history: chatHistory,
-    });
-    chatHistory.push(new schema_1.ChatMessage(response.text, "bot"));
-    // const persianRes=await translatte(response.text,{from:'en',to:'fa'});
-    // console.log(persianRes);
-    return response;
+
+  const cheaperModel = new openai_1.ChatOpenAI({
+    temperature: 0.1,
+    modelName: "gpt-3.5-turbo",
+    openAIApiKey: apiKey,
+  });
+
+  chain = chains_1.ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorStore.asRetriever(),
+    {
+      verbose: false,
+      questionGeneratorChainOptions: {llm:cheaperModel},
+      returnSourceDocuments: false,
+      qaChainOptions: {
+        type: "StuffDocumentsChain", // Customize the QA chain type
+        maxDocuments: 1, // Limit the number of documents to consider
+        maxAnswers: 3, // Limit the number of answers to return
+        answerSeparator: "\n\n", // Customize the separator between answers
+      },
+    }
+  );
+  const response = await chain.call({
+    question: initialPrompt,
+    chat_history: chatHistory,
+  });
+  chatHistory.push(new schema_1.ChatMessage(response.text, "bot"));
+
+  return response;
 };
 async function personalChatChain(prompt, isFirst) {
-    if (isFirst) {
-        try {
-            const initialPrompt =`${prompt}`;
-            chatHistory.push(new schema_1.ChatMessage(initialPrompt, "user"));
-            const response = await initializeChain(initialPrompt);
-         
-            return response;
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-    else {
-        // chatHistory.push({
-        //     role:"user",
-        //     content:prompt
-        // });
-        const enginePrompt = prompt;
-        chatHistory.push(new schema_1.ChatMessage(enginePrompt, "user"));
-        const response = await chain.call({
-            question: enginePrompt,
-            chat_history: chatHistory,
-        });
-        // chatHistory.push({
-        //     role:"bot",
-        //     content:response.text
-        // });
-        chatHistory.push(new schema_1.ChatMessage(response.text, "bot"));
+  if (isFirst) {
+    try {
+      const initialPrompt = `
+            <begin>
+            Act like Mohammad Hashemi and definitely answer the questions in Persian and only answer questions that are related to Mohammad Hashemi and his experiences, knowledge and work and avoid answering miscellaneous questions.
+            Here is the first question :
+            <end>
+            ${prompt}`;
+      chatHistory.push(new schema_1.ChatMessage(initialPrompt, "user"));
+      const response = await initializeChain(initialPrompt);
 
-        // const persianResponse =await translatte(response.text,{from:"en",to:'fa'});
-        return response;
-
-    
+      return response;
+    } catch (error) {
+      console.log(error);
     }
+  } else {
+    // chatHistory.push({
+    //     role:"user",
+    //     content:prompt
+    // });
+    const enginePrompt = prompt;
+    chatHistory.push(new schema_1.ChatMessage(enginePrompt, "user"));
+    const response = await chain.call({
+      question: enginePrompt,
+      chat_history: chatHistory,
+    });
+    // chatHistory.push({
+    //     role:"bot",
+    //     content:response.text
+    // });
+    chatHistory.push(new schema_1.ChatMessage(response.text, "bot"));
+
+    // const persianResponse =await translatte(response.text,{from:"en",to:'fa'});
+    return response;
+  }
 }
 exports.personalChatChain = personalChatChain;
